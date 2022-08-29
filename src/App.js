d8596d3bfcce3bc1a006e2d926fc4f893b0dc641
Klin-Coders <49831574+Klin-Coders@users.noreply.github.com>
2022-08-29 22:50:09 +0300

    react first class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const Counter = () => {
+    return (
+      <Fragment>
+        <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>
+          +
+        </button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCounter) => prevCounter - 1)}>
+          -
+        </button>
+        <p>Total count: {counter}</p>
+      </Fragment>
+    );
+  };
+
+  const User = ({ name, age }) => {
+    return (
+      <>
+        <p>
+          Username: {name}, age: {age}
+        </p>
+      </>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <User name="Bolaji" age={10} />
+        <User name="Von" />
+        <User name="Time is gone" />
+        <User name="test" />
+        <Counter />
       </header>
     </div>
   );
-}
+};
 
 export default App;
